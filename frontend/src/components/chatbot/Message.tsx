78081d102d70f7fc42fea86e435a9652ac7dc1b0
Mona <itsaysusernamenotallowed@gmail.com>
2026-01-14 17:52:14 -0500

    add markdown rendering ٩( ᐛ )و

--- frontend/src/components/chatbot/Message.tsx
+++ frontend/src/components/chatbot/Message.tsx
@@ -1,5 +1,7 @@
 import { cx } from "@/utils/cx";
 import { type MessageType } from '@/types/MessageType'
+import ReactMarkdown from "react-markdown";
+import remarkGfm from "remark-gfm";
 
 type MessageProps = {
   message: MessageType
@@ -7,18 +9,21 @@ type MessageProps = {
 
 const Message = ({ message }: MessageProps) => {
   return (
-      <article className='flex min-w-0 flex-1 flex-col gap-1.5'>
-        <div
-          className={cx("relative rounded-lg text-md text-primary ring-1 ring-secondary ring-inset py-4 px-6 lg:w-3/5 sm:w-4/5",
-            message.messageFrom === 'user' ?
-              "rounded-tr-none bg-primary self-end"
-              :
-              "rounded-tl-none bg-secondary"
-          )}
-        >
+    <article className='flex min-w-0 flex-1 flex-col gap-1.5 w-min'>
+      <div
+        className={cx("relative rounded-lg text-md text-primary ring-1 ring-secondary ring-inset py-4 px-6 lg:w-3/5 sm:w-4/5 prose",
+          message.messageFrom === 'user' ?
+            "rounded-tr-none bg-primary self-end"
+            :
+            "rounded-tl-none bg-secondary"
+        )}
+      >
+        <ReactMarkdown remarkPlugins={[remarkGfm]}>
           {message.text}
-        </div>
-      </article>
+        </ReactMarkdown>
+
+      </div>
+    </article>
   )
 }
 
